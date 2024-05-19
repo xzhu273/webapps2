b7e809b9198abcd49c478f8360c37a6eb5d99aa8
xzhu273 <165421031+xzhu273@users.noreply.github.com>
2024-05-19 14:03:08 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources "tacos"
+  resources "dice"
+  resources "cards"
+  resources "bitcoins"
+  resources "companies"
+  resources "contacts"
+  resources "posts"
+end
